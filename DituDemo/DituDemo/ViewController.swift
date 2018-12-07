8aff2b994fc8baa86e5f31ff73eef243038f84af
jackyshan <jackyshan@email.com>
2018-12-07 21:31:32 +0800

    高德SDK组件封装

---
+++ DituDemo/DituDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DituDemo
+//
+//  Created by jackyshan on 2018/12/7.
+//  Copyright © 2018年 Jacky Labs. All rights reserved.
+//
+
+import UIKit
+import GDSDK
+
+class ViewController: UIViewController {
+    
+    var mapView: MAMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GDSDK/GDSDK/GDSDK.h
@@ -0,0 +1,24 @@
+//
+//  GDSDK.h
+//  GDSDK
+//
+//  Created by jackyshan on 2018/12/7.
+//  Copyright © 2018年 Jacky Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MAMapKit/MAMapKit.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
+#import <AMapSearchKit/AMapSearchKit.h>
+#import <AMapLocationKit/AMapLocationKit.h>
+#import <AMapNaviKit/AMapNaviKit.h>
+
+//! Project version number for GDSDK.
+FOUNDATION_EXPORT double GDSDKVersionNumber;
+
+//! Project version string for GDSDK.
+FOUNDATION_EXPORT const unsigned char GDSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GDSDK/PublicHeader.h>
+
+
